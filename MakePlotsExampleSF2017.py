276ab2c4e9aaf1ec91f931417d405e3ac1b810b7
gael perrin <gael.ludovic.perrin@cern.ch>
2018-02-20 17:38:32 +0100

    .

--- MakePlotsExampleSF2017.py
+++ MakePlotsExampleSF2017.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     ##################
 
 
-    ID = [  'TnP_MC_NUM_LooseID_DEN_genTracks_PAR_pt_eta.root',
-            'TnP_MC_NUM_TightID_DEN_genTracks_PAR_pt_eta.root',
-            'TnP_MC_NUM_MediumID_DEN_genTracks_PAR_pt_eta.root',
-            'TnP_MC_NUM_HighPtID_DEN_genTracks_PAR_newpt_eta.root',
-            'TnP_MC_NUM_TrkHighPtID_DEN_genTracks_PAR_newpt_eta.root',
-            'TnP_MC_NUM_SoftID_DEN_genTracks_PAR_pt_eta.root',
-            'TnP_MC_NUM_MediumPromptID_DEN_genTracks_PAR_pt_eta.root'
-            ]
+    #ID = [  'TnP_MC_NUM_LooseID_DEN_genTracks_PAR_pt_eta.root',
+    #        'TnP_MC_NUM_TightID_DEN_genTracks_PAR_pt_eta.root',
+    #        'TnP_MC_NUM_MediumID_DEN_genTracks_PAR_pt_eta.root',
+    #        'TnP_MC_NUM_HighPtID_DEN_genTracks_PAR_newpt_eta.root',
+    #        'TnP_MC_NUM_TrkHighPtID_DEN_genTracks_PAR_newpt_eta.root',
+    #        'TnP_MC_NUM_SoftID_DEN_genTracks_PAR_pt_eta.root',
+    #        'TnP_MC_NUM_MediumPromptID_DEN_genTracks_PAR_pt_eta.root'
+    #        ]
 
     ISO = [ 'TnP_MC_NUM_TightRelIso_DEN_MediumID_PAR_pt_eta.root',
             'TnP_MC_NUM_LooseRelIso_DEN_MediumID_PAR_pt_eta.root',
@@ -32,12 +32,20 @@ if __name__ == "__main__":
             'TnP_MC_NUM_LooseRelIso_DEN_TightIDandIPCut_PAR_pt_eta.root',
             'TnP_MC_NUM_TightRelTkIso_DEN_HighPtIDandIPCut_PAR_newpt_eta.root']
 
+    ID = [  'TnP_MC_NUM_LooseID_DEN_genTracks_PAR_pt_eta.root']
+    #ID = ['TnP_MC_NUM_LooseID_DEN_genTracks_PAR_pt_eta.root']
+    #ID = ['TnP_MC_NUM_TightID_DEN_genTracks_PAR_pt_eta.root']
+    #ID = ['TnP_MC_NUM_MediumID_DEN_genTracks_PAR_pt_eta.root']
+    #ID = ['TnP_MC_NUM_HighPtID_DEN_genTracks_PAR_newpt_eta.root']
+    #ID = ['TnP_MC_NUM_TrkHighPtID_DEN_genTracks_PAR_newpt_eta.root']
+    #ID = ['TnP_MC_NUM_SoftID_DEN_genTracks_PAR_pt_eta.root']
+    #ID = ['TnP_MC_NUM_MediumPromptID_DEN_genTracks_PAR_pt_eta.root']
 
     hr_data_List = []
 
     #for Id in ID+ISO:
-    #for Id in ID:
-    for Id in ISO:
+    for Id in ID:
+    #for Id in ISO:
     
 
         print 'Readind'
@@ -50,7 +58,7 @@ if __name__ == "__main__":
 
          
         #The argument in the HistoPloter is the path where the plots are going to be stored
-        hp = HistoPloter('.')
+        hppath = '.'
 
         ##############
         #DATA
@@ -73,6 +81,21 @@ if __name__ == "__main__":
         hr_dataBC.setType('data')       #(optional) Can be either 'data' or 'MC'. Used for legend
 
         if isid:
+            fileBC = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_BC/MC_mcidBC/'+Id
+        else:
+            fileF = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyISO_BC/MC_mcidBC/'+Id
+        hr_mcBC = HistoReader('MCBC')
+        hr_mcBC.readfile(fileBC)      
+        hr_mcBC.SetNewRange(20, 120)  
+        hr_mcBC.CleanBigError(0.05)   
+        hr_mcBC.setLumi(14.432)       
+        hr_mcBC.setInfo('BC')         
+        hr_mcBC.setType('mc')
+        hp = HistoPloter(hppath)
+        hp.PlotEff1D([hr_dataBC, hr_mcBC])
+        del hp
+
+        if isid:
            fileDE = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_DE/DATA_dataidDE/'+Id
         else: 
            fileDE = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyISO_DE/DATA_dataidDE/'+Id
@@ -84,6 +107,24 @@ if __name__ == "__main__":
         hr_dataDE.setInfo('DE')
         hr_dataDE.setType('data')
         hr_dataBC.Sum(hr_dataDE)
+
+        if isid:
+            fileDE = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_DE/MC_mcidDE/'+Id
+        else:
+            fileDE = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyISO_DE/MC_mcidDE/'+Id
+        hr_mcDE = HistoReader('MCDE')
+        hr_mcDE.readfile(fileDE)
+        hr_mcDE.SetNewRange(20, 120)
+        hr_mcDE.CleanBigError(0.05)
+        hr_mcDE.setLumi(13.503)
+        hr_mcDE.setInfo('DE')
+        hr_mcDE.setType('mc')
+
+        hp = HistoPloter(hppath)
+        hp.PlotEff1D([hr_dataDE, hr_mcDE])
+        del hp
+
+        hr_mcBC.Sum(hr_mcDE)
 
         if isid:
             fileF = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_F/DATA_dataidF/'+Id
@@ -100,32 +141,6 @@ if __name__ == "__main__":
         hr_dataBC.Sum(hr_dataF) #The efficiency distributions in hr_dataBC are now the lumi-reweigted sum of hr_dataBC, hr_dataDE, hr_dataF
  
         if isid:
-            fileBC = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_BC/MC_mcidBC/'+Id
-        else:
-            fileF = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyISO_BC/MC_mcidBC/'+Id
-        hr_mcBC = HistoReader('MCBC')
-        hr_mcBC.readfile(fileBC)      
-        hr_mcBC.SetNewRange(20, 120)  
-        hr_mcBC.CleanBigError(0.05)   
-        hr_mcBC.setLumi(14.432)       
-        hr_mcBC.setInfo('BC')         
-        hr_mcBC.setType('mc')
-
-
-        if isid:
-            fileDE = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_DE/MC_mcidDE/'+Id
-        else:
-            fileDE = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyISO_DE/MC_mcidDE/'+Id
-        hr_mcDE = HistoReader('MCDE')
-        hr_mcDE.readfile(fileDE)
-        hr_mcDE.SetNewRange(20, 120)
-        hr_mcDE.CleanBigError(0.05)
-        hr_mcDE.setLumi(13.503)
-        hr_mcDE.setInfo('DE')
-        hr_mcDE.setType('mc')
-        hr_mcBC.Sum(hr_mcDE)
-
-        if isid:
             fileF = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyID_F/MC_mcidF/'+Id
         else:
             fileF = '/afs/cern.ch/user/f/fernanpe/public/for_Gael/Efficiencies_2017/EfficiencyISO_F/MC_mcidF/'+Id
@@ -137,8 +152,14 @@ if __name__ == "__main__":
         hr_mcF.setLumi(13.433)
         hr_mcF.setInfo('F')
         hr_mcF.setType('mc')
+
+        hp = HistoPloter(hppath)
+        hp.PlotEff1D([hr_dataF, hr_mcF])
+        del hp
+
         hr_mcBC.Sum(hr_mcF) #The efficiency distributions in hr_mcF are now the lumi-reweigted sum of hr_mcBC, hr_mcDE, hr_mcF
 
-        #Make 1D efficiency plots for multiple sample
-        hp.PlotEff1D([hr_dataBC, hr_mcBC])
+        ##Make 1D efficiency plots for multiple sample
+        #hp = HistoPloter(hppath)
+        #hp.PlotEff1D([hr_dataBC, hr_mcBC])
 
